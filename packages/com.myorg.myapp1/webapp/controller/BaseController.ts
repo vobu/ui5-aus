3415cca7f7bde02012df55c199abba25b1ea8775
Volker Buzek <volker.buzek@js-soft.com>
2023-05-26 18:38:52 +0200

    feat: add model data for direct access

--- packages/com.myorg.myapp1/webapp/controller/BaseController.ts
+++ packages/com.myorg.myapp1/webapp/controller/BaseController.ts
@@ -7,6 +7,14 @@ import ResourceBundle from "sap/base/i18n/ResourceBundle";
  * @namespace com.myorg.myapp1.controller
  */
 export default abstract class BaseController extends Controller {
+	uiModelData: any;
+
+	onInit(): void {
+		super.onInit();
+		// requires manifest => observe=true!
+		// -> only properties are allowed to be changed, no object/array refs!
+		this.uiModelData = this.getAppComponent().getModel("viewModel").getData();
+	}
 	/**
 	 * Convenience method for accessing the component of the controller's view.
 	 * @returns The component of the controller's view
